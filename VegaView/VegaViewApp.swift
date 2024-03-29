12547b9996e9d9e0d760335f396555c9e5f6e15c
Arnold deVos <adv+gh@backgroundsignal.com>
2024-03-29 16:38:32 +1100

    Initial Commit

---
+++ VegaView/VegaViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  VegaViewApp.swift
+//  VegaView
+//
+//  Created by Arnold Devos on 29/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct VegaViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
